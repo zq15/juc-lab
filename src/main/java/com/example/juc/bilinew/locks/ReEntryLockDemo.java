631a59313196502d0508ba391a72b3abf0a85f55
zds <1529219484@qq.com>
2024-09-12 14:32:48 +0800

    可重入锁

---
+++ src/main/java/com/example/juc/bilinew/locks/ReEntryLockDemo.java
@@ -0,0 +1,55 @@
+package com.example.juc.bilinew.locks;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+// 可重入锁案例
+public class ReEntryLockDemo {
+    public static void main(String[] args) {
+        final Object o = new Object();
+        /**
+         * ---------------外层调用
+         * ---------------中层调用
+         * ---------------内层调用
+         */
+        new Thread(() -> {
+            synchronized (o) {
+                System.out.println("---------------外层调用");
+                synchronized (o) {
+                    System.out.println("---------------中层调用");
+                    synchronized (o) {
+                        System.out.println("---------------内层调用");
+                    }
+                }
+            }
+        }, "t1").start();
+
+        /**
+         * 注意：加锁几次就需要解锁几次
+         * ---------------外层调用
+         * ---------------中层调用
+         * ---------------内层调用
+         */
+        Lock lock = new ReentrantLock();
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println("---------------外层调用");
+                lock.lock();
+                try {
+                    System.out.println("---------------中层调用");
+                    lock.lock();
+                    try {
+                        System.out.println("---------------内层调用");
+                    } finally {
+                        lock.unlock();
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            } finally {
+                lock.unlock();
+            }
+        }, "t2").start();
+    }
+}
