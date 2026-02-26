3a60a4a0fc3f60fc194cef8e52a357e557dd36a3
zq15 <1529219484@qq.com>
2026-02-26 22:43:35 +0800

    补充说明 @EnableAsync 的影响

--- src/main/java/com/example/juc/bilinew/springasync/AppConfig.java
+++ src/main/java/com/example/juc/bilinew/springasync/AppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 /**
  * 不注册自定义 AsyncUncaughtExceptionHandler
  * Spring 默认使用 SimpleAsyncUncaughtExceptionHandler，对 void 方法的异常只打日志
+ *
+ * 如果不开启 @EnableAsync 相当于串行
  */
 @Configuration
 @EnableAsync
